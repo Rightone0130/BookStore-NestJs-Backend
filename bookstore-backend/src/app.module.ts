3b7b5fac1cd4692938a0ddf44125c29780a4f18a
rightone0130 <rightone0130@gmail.com>
2023-11-03 23:18:16 +0100

    feat: added graphql dependencies and configurations added user resource folder

---
+++ bookstore-backend/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schma.gql'),
+      sortSchema: true,
+    }),
+    UsersModule
+  ],
+})
+export class AppModule {}
