452fcbb100b2cb7b61f05bd29cf3cc192f3a3caf
rightone0130 <rightone0130@gmail.com>
2023-11-06 22:07:45 +0100

    fiX: troubleshooting cors bug

--- bookstore-backend/src/main.ts
+++ bookstore-backend/src/main.ts
@@ -1,20 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
+import * as cors from 'cors'
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   
-  const expressApp = express();
-  expressApp.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
-
-  app.use(expressApp);
+  app.use(cors)
 
   await app.listen(3000);
 }
